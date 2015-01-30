0e9bf4b457d18757a3444470f3344a7046534d36
BG Comedian <umbrelian33@gmail.com>
2015-01-30 16:00:18 +0700

    v.1

--- app/src/main/java/th/ac/tu/siit/its333/lab2exercise1/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab2exercise1/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
 
     // expr = the current string to be calculated
     StringBuffer expr;
+    int temp=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void updateExprDisplay() {
-        TextView tvExpr = (TextView)findViewById(R.id.tvExpr);
+        TextView tvExpr = (TextView) findViewById(R.id.tvExpr);
         tvExpr.setText(expr.toString());
     }
 
@@ -36,11 +37,39 @@ public class MainActivity extends ActionBarActivity {
         //reference: http://stackoverflow.com/questions/2206378/how-to-split-a-string-but-also-keep-the-delimiters
         String e = expr.toString();
         String[] tokens = e.split("((?<=\\+)|(?=\\+))|((?<=\\-)|(?=\\-))|((?<=\\*)|(?=\\*))|((?<=/)|(?=/))");
+
+        String op = "";
+        int value;
+        value = Integer.parseInt(tokens[0]);
+
+        for(int i =1;i<tokens.length-1;i++){
+
+            switch(tokens[i]){
+                case "+":
+                    value=value+Integer.parseInt(tokens[i+1]);
+                    break;
+
+                case "-":
+                    value=value-Integer.parseInt(tokens[i+1]);
+                    break;
+
+                case "*":
+                    value=value*Integer.parseInt(tokens[i+1]);
+                    break;
+
+                case "/":
+                    value=value/Integer.parseInt(tokens[i+1]);
+                    break;
+
+            }
+        }
+        TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        tvAns.setText(Integer.toString(value));
     }
 
     public void digitClicked(View v) {
         //d = the label of the digit button
-        String d = ((TextView)v).getText().toString();
+        String d = ((TextView) v).getText().toString();
         //append the clicked digit to expr
         expr.append(d);
         //update tvExpr
@@ -53,7 +82,33 @@ public class MainActivity extends ActionBarActivity {
         //IF the last character in expr is not an operator and expr is not "",
         //THEN append the clicked operator and updateExprDisplay,
         //ELSE do nothing
+        String e = expr.toString();
+        String[] tokens = e.split("((?<=\\+)|(?=\\+))|((?<=\\-)|(?=\\-))|((?<=\\*)|(?=\\*))|((?<=/)|(?=/))");
+
+        if (!(tokens[tokens.length - 1].equals("")) &&
+                (!tokens[tokens.length - 1].equals("+")) &&
+                (!tokens[tokens.length - 1].equals("-")) &&
+                (!tokens[tokens.length - 1].equals("*")) &&
+                (!tokens[tokens.length - 1].equals("/"))) {
+            String d = ((TextView) v).getText().toString();
+            expr.append(d);
+            updateExprDisplay();
+        } else {
+        }
+
     }
+
+
+    public void equalClicked(View v) {
+      TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        String output = tvAns.getText().toString();
+        tvAns.setText(output);
+        expr = new StringBuffer();
+        expr.append(output);
+        updateExprDisplay();
+
+    }
+
 
     public void ACClicked(View v) {
         //Clear expr and updateExprDisplay
@@ -68,11 +123,58 @@ public class MainActivity extends ActionBarActivity {
     public void BSClicked(View v) {
         //Remove the last character from expr, and updateExprDisplay
         if (expr.length() > 0) {
-            expr.deleteCharAt(expr.length()-1);
+            expr.deleteCharAt(expr.length() - 1);
             updateExprDisplay();
         }
     }
 
+    public void MClicked(View v){
+        int id = v.getId();
+        TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        Toast t;
+        switch (id){
+            case R.id.madd:
+                temp = temp+Integer.parseInt(tvAns.getText().toString());
+                t = Toast.makeText(this.getApplicationContext(),"M is "+ temp
+                        ,Toast.LENGTH_SHORT);
+                t.show();
+                break;
+            case R.id.msub:
+                temp = temp-Integer.parseInt(tvAns.getText().toString());
+                t = Toast.makeText(this.getApplicationContext(),"M is "+ temp
+                        ,Toast.LENGTH_SHORT);
+                t.show();
+                break;
+            case R.id.mr:
+                TextView tvExpr = (TextView)findViewById(R.id.tvExpr);
+                tvExpr.setText(temp+"");
+                tvAns.setText(temp+"");
+                t = Toast.makeText(this.getApplicationContext(),"M is "+ temp
+                        ,Toast.LENGTH_SHORT);
+                t.show();
+                break;
+            case R.id.mc:
+                temp = 0;
+                tvAns.setText("0");
+                t = Toast.makeText(this.getApplicationContext(),"M is cleared"
+                        ,Toast.LENGTH_SHORT);
+                t.show();
+                break;
+
+        }
+    }
+
+    /*
+  TextView tv = (TextView)findViewById(R.id.tvOutput);
+  tv.setText(Integer.toString(counter));
+
+  if (counter % 10 == 0) {
+    Toast t = Toast.makeText(this.getApplicationContext(),
+    "The counter is " + counter,
+    Toast.LENGTH_SHORT);
+    t.show();
+  }
+}*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
